8cdbd058e341bb474ed07745dd7b9d4888999ba5
Victor Higaki <victorhigaki@outlook.com>
2025-05-19 01:31:07 -0300

    Implementando Schema Registry

--- Desenvolvedor.IO/Consumidor/Program.cs
+++ Desenvolvedor.IO/Consumidor/Program.cs
@@ -1,4 +1,14 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ConsumerConfig
 {
@@ -6,13 +16,15 @@ var config = new ConsumerConfig
     BootstrapServers = "localhost:9092"
 };
 
-using var consumer = new ConsumerBuilder<string, string>(config).Build();
+using var consumer = new ConsumerBuilder<string, desenvolvedor.io.Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<desenvolvedor.io.Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
 
-consumer.Subscribe("topico-teste");
+consumer.Subscribe("cursos");
 
 while (true)
 {
     var result = consumer.Consume();
 
-    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+    Console.WriteLine($"Mensagem: {result.Message.Value.Descricao}");
 }
--- Desenvolvedor.IO/Produtor/Program.cs
+++ Desenvolvedor.IO/Produtor/Program.cs
@@ -1,15 +1,30 @@
 ﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
 
-using var producer = new ProducerBuilder<string, string>(config).Build();
+using var producer = new ProducerBuilder<string, desenvolvedor.io.Curso>(config)
+    .SetValueSerializer(new AvroSerializer<desenvolvedor.io.Curso>(schemaRegistry))
+    .Build();
 
-var message = new Message<string, string>
+var message = new Message<string, desenvolvedor.io.Curso>
 {
     Key = Guid.NewGuid().ToString(),
-    Value = $"Mensagem teste {DateTime.Now.Second}"
+    Value = new desenvolvedor.io.Curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = "Curso de Apache Kafka"
+    }
 };
 
-var result = await producer.ProduceAsync("topico-teste", message);
+var result = await producer.ProduceAsync("cursos", message);
 
 Console.WriteLine($"{result.Offset}");
