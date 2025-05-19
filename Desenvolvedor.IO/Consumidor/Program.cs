5bd5ef76f640e99d83fa27bc4d04853ff6473838
Victor Higaki <victorhigaki@outlook.com>
2025-05-18 23:13:02 -0300

    Implementando um produtor e um consumidor

--- Desenvolvedor.IO/Consumidor/Program.cs
+++ Desenvolvedor.IO/Consumidor/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("topico-teste");
+
+while (true)
+{
+    var result = consumer.Consume();
+
+    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
